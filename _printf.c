409a628653ec9fd7a27dabb849dcc77a93d51bbe
Mikoz999 <mikozotieno@gmail.com>
2023-06-24 03:37:37 -0700

    updating _printf.c file to compile with gcc and betty coding style

--- _printf.c
+++ _printf.c
@@ -1,131 +1,81 @@
 #include <stdarg.h>
-#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+
+int _print_string(char *str);
 
 /**
- * _printf - prints formatted output
- * @format: format string
+ * _printf - Produces output according to a format
+ * @format: The format string
  *
- * Return: number of characters printed
+ * Return: The number of characters printed (excluding the null byte)
  */
 int _printf(const char *format, ...)
 {
- va_list args;
- int printed_chars;
+	va_list args;
+	int count = 0;
 
- printed_chars = 0;
- va_start(args, format);
- if (format)
-  printed_chars = parse_and_print(format, args);
- va_end(args);
+	va_start(args, format);
 
- return (printed_chars);
+	if (format == NULL)
+		return (-1);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == '\0')
+				return (-1);
+			switch (*format)
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += _print_string(va_arg(args, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar('%');
+					count += _putchar(*format);
+					break;
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+
+		format++;
+	}
+
+	va_end(args);
+
+	return (count);
 }
 
 /**
- * parse_and_print - parses and prints the format string
- * @format: format string
- * @args: variable argument list
+ * _print_string - Prints a string
+ * @str: The string to be printed
  *
- * Return: number of characters printed
+ * Return: The number of characters printed
  */
-int parse_and_print(const char *format, va_list args)
+int _print_string(char *str)
 {
- int i, j, printed_chars;
- char *str;
- print_func_t print_func;
- specifier_t specifiers[] = {
-  {"c", print_char},
-  {"s", print_string},
-  {"%", print_percent},
-  {NULL, NULL}
- };
+	int count = 0;
 
- i = 0;
- printed_chars = 0;
- while (format && format[i])
- {
-  if (format[i] == '%')
-  {
-   i++;
-   print_func = get_print_func(format[i], specifiers);
-   if (print_func)
-   {
-    j = i + 1;
-    while (format[j] && !is_specifier(format[j]))
-     j++;
-    str = get_arg_str(format + i, j - i, args);
-    if (str)
-    {
-     printed_chars += print_func(str);
-     free(str);
-    }
-    i = j;
-   }
-   else if (format[i])
-   {
-    _putchar(format[i]);
-    printed_chars++;
-   }
-  }
-  else
-  {
-   _putchar(format[i]);
-   printed_chars++;
-  }
-  i++;
- }
+	if (str == NULL)
+		str = "(null)";
 
- return (printed_chars);
-}
+	while (*str)
+	{
+		count += _putchar(*str);
+		str++;
+	}
 
-/**
- * get_print_func - gets the appropriate print function for a specifier
- * @specifier: specifier character
- * @specifiers: array of specifier_t structs
- *
- * Return: pointer to print function, or NULL if specifier not found
- */
-print_func_t get_print_func(char specifier, specifier_t *specifiers)
-{
- int i;
-
- for (i = 0; specifiers[i].specifier; i++)
- {
-  if (specifiers[i].specifier[0] == specifier)
-   return (specifiers[i].print_func);
- }
-
- return (NULL);
-}
-
-/**
- * get_arg_str - gets the string argument for a specifier
- * @format: format string
- * @length: length of specifier
- * @args: variable argument list
- *
- * Return: pointer to string argument, or NULL if specifier not found
- */
-char *get_arg_str(const char *format, int length, va_list args)
-{
- if (length == 1)
-  return (get_char_arg(args));
- else if (length == 2)
-  return (get_str_arg(args));
- else if (length == 0)
-  return (strdup(""));
- else
-  return (NULL);
-}
-
-/**
- * is_specifier - checks if a character is a valid specifier
- * @c: character to check
- *
- * Return: 1 if specifier, 0 otherwise
- */
-int is_specifier(char c)
-{
- return (c == 'c' || c == 's' || c == '%');
+	return (count);
 }
 
